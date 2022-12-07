c07992a92afa11e78e2427059761d50e8d4a4702
lismgmk <lismgmk2@gmail.com>
2022-12-07 19:11:01 +0300

    add config options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import Joi from 'joi';
-import { BlackList } from './entities/black-list.entity';
-import { User } from './entities/user.entity';
+import { configRoot } from './config/configuration';
+import { typeOrmConfigAsync } from './config/ormconfig';
 import { AuthModule } from './modules/auth/auth.module';
 import { BlackListModule } from './modules/black-list/black-list.module';
 import { JwtPassModule } from './modules/jwt-pass-service/jwt-pass.module';
@@ -11,31 +10,8 @@ import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('POSTGRES_HOST'),
-        port: +configService.get<string>('POSTGRES_PORT'),
-        username: configService.get<string>('POSTGRES_USER'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        database: configService.get<string>('POSTGRES_DB'),
-        entities: [User, BlackList],
-        synchronize: true,
-        autoLoadEntities: true,
-      }),
-      inject: [ConfigService],
-    }),
+    ConfigModule.forRoot(configRoot),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     UsersModule,
     AuthModule,
     JwtPassModule,
---
+++ src/config/ormconfig.ts
@@ -0,0 +1,30 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { BlackList } from '../entities/black-list.entity';
+import { User } from '../entities/user.entity';
+
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: configService.get<string>('POSTGRES_HOST'),
+      port: parseInt(configService.get<string>('POSTGRES_PORT')),
+      username: configService.get<string>('POSTGRES_USER'),
+      password: configService.get<string>('POSTGRES_PASSWORD'),
+      database: configService.get<string>('POSTGRES_DB'),
+      entities: [User, BlackList],
+      synchronize: true,
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  inject: [ConfigService],
+};
